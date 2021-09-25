7a8a8cdd2f4cf5ac89680ac4f34a7e1e053542d2
Artem Meleshchenko <ameleshchenko@myseneca.ca>
2021-09-25 16:31:52 -0400

    Revert "Delete its/src directory"
    This reverts commit f39420ca71a283b0b88b5cc561dadc4dc73dc64c.

---
+++ its/src/App.js
@@ -0,0 +1,10 @@
+import LoginForm from "./LogInComponent/LoginForm";
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+       <LoginForm /> 
+  );
+}
+
+export default App;
