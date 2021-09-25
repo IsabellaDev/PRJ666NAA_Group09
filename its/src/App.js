e2f01ece40fd01482af88db8a6ed33b60b273f86
nsheikh12 <nsheikh12@myseneca.ca>
2021-09-25 14:15:45 -0400

    added sidebar

--- its/src/App.js
+++ its/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navigation';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import Install from './pages/Install'
+import ReportIssue from './pages/ReportIssue';
+import Articles from './pages/Articles';
+import RequestService from './pages/requestService';
+import Contact from './pages/contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/install' component={Install}/>
+        <Route path='/reportIssue' component={ReportIssue}/>
+        <Route path='/articles' component={Articles}/>
+        <Route path='/requestService' component={RequestService}/>
+        <Route path='/contact' component={Contact}/>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
